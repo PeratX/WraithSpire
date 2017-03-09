ad9a72de3d2069b62b2ee7dfc9af913f60a0d6b3
PeratX <1215714524@qq.com>
2017-03-09 17:00:50 +0800

    Resolver improved.

--- src/PeratX/WraithSpire/WraithSpire.php
+++ src/PeratX/WraithSpire/WraithSpire.php
@@ -18,6 +18,7 @@ use PeratX\SimpleFramework\Console\TextFormat;
 use PeratX\SimpleFramework\Framework;
 use PeratX\SimpleFramework\Module\Module;
 use PeratX\SimpleFramework\Module\ModuleDependencyResolver;
+use PeratX\SimpleFramework\Module\ModuleInfo;
 use PeratX\SimpleFramework\Util\Config;
 use PeratX\SimpleFramework\Util\Util;
 
@@ -66,8 +67,54 @@ class WraithSpire extends Module implements ModuleDependencyResolver{
 		}
 	}
 
-	public function downloadDependency(string $name, string $version){
+	public function downloadDependency(string $moduleName, string $name, string $version): bool{
+		$vendor = "";
+		if(strstr($name, "/")){
+			$rName = explode("/", $name, 2);
+			$name = $rName[1];
+			$vendor = $rName[0];
+		}
+		if($vendor == ""){
+			Logger::info(TextFormat::RED . $moduleName . " requires dependency $name does not have a vendor, please contact the author of the module or manually resolve its dependency.");
+			return false;
+		}
+		if(($module = $this->framework->getModule($name)) instanceof Module){
+			if($module->getInfo()->getLoadMethod() == ModuleInfo::LOAD_METHOD_SOURCE){
+				Logger::info(TextFormat::RED . "Please manually remove the source folder of " . $module->getInfo()->getName() . " then the dependency resolver can download the specifying module.");
+				return false;
+			}
+			rename($module->getFile(), $module->getFile() . ".old");
+		}
+		if(isset($this->database[$vendor][$name][$version])){
+			$link = $this->database[$vendor][$name][$version];
+			$fileName = explode("/", $link);
+			$fileName = end($fileName);
+			self::downloadFile($this->getFramework()->getModulePath() . $fileName, $link);
+			return $this->getFramework()->tryloadModule($this->getFramework()->getModulePath() . $fileName);
+		}
+		return false;
+	}
 
+	public static function downloadFile(string $file, string $url){
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, ["User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"]);
+		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+		curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 86400);//1 Day
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_BUFFERSIZE, 20971520);//20M
+		$ret = curl_exec($ch);
+		curl_close($ch);
+
+		if($ret != false){
+			file_put_contents($file, $ret, FILE_BINARY);
+		}
 	}
 
 	public function resolveDependency(Module $module){
@@ -83,8 +130,8 @@ class WraithSpire extends Module implements ModuleDependencyResolver{
 			if(count($version) != 3){
 				$error = true;
 			}
-			if(($module = $this->framework->getModule($name)) instanceof Module){
-				$targetVersion = explode(".", $module->getInfo()->getVersion());
+			if(($dependencyModule = $this->framework->getModule($name)) instanceof Module){
+				$targetVersion = explode(".", $dependencyModule->getInfo()->getVersion());
 				if(count($targetVersion) != 3){
 					$error = true;
 				}
@@ -100,9 +147,10 @@ class WraithSpire extends Module implements ModuleDependencyResolver{
 				$error = true;
 			}
 			if($error == true){
-				Logger::error("Module " . '"' . $this->getInfo()->getName() . '"' . " requires dependency module " . '"' . $name . '"' . " version " . $dependency["version"] . ". Resolving dependency...");
-				$this->downloadDependency($dependency["name"], $dependency["version"]);
-				return false;
+				Logger::error("Module " . '"' . $module->getInfo()->getName() . '"' . " requires dependency module " . '"' . $name . '"' . " version " . $dependency["version"] . ". Resolving dependency...");
+				if(!$this->downloadDependency($module->getInfo()->getName(), $dependency["name"], $dependency["version"])){
+					return false;
+				}
 			}
 		}
 		return true;
